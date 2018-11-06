b8a0b5b2432eea591cec1411a03dc9f7ea4bed91
Raymond Mashigo <raymond.mashigo@mondiamedia.com>
2018-11-06 11:23:35 +0200

    Ray - added filter component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Filter extends React.Component {
-    render() {
-        return (
-            <div>
-                <input type="text" placeholder="Filter"/>
-                <button className="btn btn-default" type="submit"><i className="glyphicon glyphicon-search"></i>
-                </button>
-            </div>
-        );
-    }
-}
 
 const Header = (props) => (
     <nav className="header navbar navbar-dark bg-dark">
         <div className="container">
             <div className="row m-auto">
-                <i className="fa fa-film fa-2x text-white my-auto">i</i>
+                <i className="fa fa-film fa-2x text-white my-auto"></i>
                 <div className="h3 ml-3 my-auto text-light" href="/">{props.title}</div>
             </div>
-            <Filter/>
         </div>
     </nav>
 );
--- src/components/Movie/MovieList.js
+++ src/components/Movie/MovieList.js
@@ -1,13 +1,54 @@
 import PropTypes from 'prop-types';
-import React, { } from 'react';
+import React, {} from 'react';
 import MovieCard from './MovieCard';
+
+class Filter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            search: '',
+        };
+    }
+
+    updateSearch(event) {
+        this.setState({search: event.target.value.substr(0, 20)});
+    }
+
+    render() {
+        let FilteredMovies = this.props.movies.filter((movie) => {
+            return movie.title.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
+        });
+        console.log('star wars', FilteredMovies);
+        return (
+            <div className="filter-list">
+                <form>
+                    <fieldset className="form-group">
+                        <input type="text"
+                            className="form-control form-control-lg" placeholder="Search"
+                            value={this.state.search} onChange={this.updateSearch.bind(this)}/>
+                    </fieldset>
+                </form>
+                <br/>
+                <br/>
+            </div>
+
+        );
+    }
+}
+
+Filter.propTypes = {
+    movies: PropTypes.array
+};
 
 const getMovies = (movies) => {
     return (
-        <div className="card-deck">
-            {
-                movies.map(movie => <MovieCard key={movie.id} movie={movie}/>)
-            }
+        <div>
+            <Filter movies={movies} onTextChange={text => this.setState({filteredString: text})}/>
+            <div className='card-deck'>
+                {
+                    movies.map(movie => <MovieCard key={movie.id} movie={movie}/>)
+                }
+            </div>
         </div>
     );
 };
